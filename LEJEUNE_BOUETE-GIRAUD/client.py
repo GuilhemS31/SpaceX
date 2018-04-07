1f1991ac0e028494d4019a8b6fcc34c19db1fc2d
Nicolas Lejeune <nicolas.lejeune31@gmail.com>
2018-04-07 13:28:45 +0200

    CLIENT: ligne de commande

--- LEJEUNE_BOUETE-GIRAUD/client.py
+++ LEJEUNE_BOUETE-GIRAUD/client.py
@@ -4,3 +4,16 @@ import sys
 if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} <ip> <port>")
     sys.exit(1)
+
+TAILLE_TAMPON = 256
+
+print("Enter a command (help to see the list, quit to leave the program) : ")
+with socket(AF_INET, SOCK_DGRAM) as sock:
+    while True:
+        message = input("> ")
+        if message == "quit" :
+            print("Disconnected successful")
+            break
+        sock.sendto(message.encode(), (sys.argv[1], int(sys.argv[2])))
+        reponse, _ = sock.recvfrom(TAILLE_TAMPON)
+        print(reponse.decode())
