bef2ad241ccaac3642316bd0be9c903efd276b50
GuilhemS31 <guilhem31.susa@hotmail.com>
2018-04-12 21:08:02 +0200

    send pas coder, pause unpause move marchent pas encore

---
+++ SIBADE-SUSA/lib.py
@@ -0,0 +1,131 @@
+import random 
+
+class Tiles:
+	def __init__(self,x,y,obs):
+		self.__posX = x            
+		self.__posY = y         
+		self.__isObs = obs   # true is obstacle    
+		self.__isRess = False  # true is ressource  
+		
+	@property
+	def posX(self) : return self.__posX
+	@posX.setter
+	def posX(self,aj) : self.__posX = self.__posX + aj
+	@property
+	def posY(self) : return self.__posY
+	@posY.setter
+	def posY(self,aj) : self.__posY = self.__posY + aj
+	@property
+	def isObs(self) : return self.__isObs
+	@property
+	def isRess(self) : return self.__isRess
+	@isRess.setter
+	def isRess(self,isR) : self.__isRess = isR 
+
+	def __str__(self):
+		if self.isObs :
+			return "X"
+		elif self.isRess :
+			return "O"
+		else:
+			return " "
+		
+class Robot():
+	def __init__(self,x,y,name):
+		self.__posX = x            
+		self.__posY = y
+		self.__name = name
+		self.__ress = 0
+		self.__actif = True
+		
+	@property
+	def posX(self) : return self.__posX
+	@property
+	def posY(self) : return self.__posY
+	@property
+	def name(self) : return self.__name
+	@name.setter
+	def name(self,newName) : self.__name = newName
+	@property
+	def ress(self) : return self.__ress
+	@ress.setter
+	def ress(self) : self.__ress += 1
+	@property
+	def actif(self) : return self.__actif 
+	@actif.setter
+	def actif(self,mode) : self.__actif = mode
+		
+	def __str__(self):
+		status = ""
+		if self.actif :
+			status = "Actif"
+		else:
+			status = "Inactif" 
+		ret = self.name + "("+ status +")"+" : [" + str(self.posX) + "," + str(self.posY) + "] - " + str(self.ress) + " ressource(s)"
+		return ret
+        
+class Map:
+	
+	def refreshRess(self):
+		cpt = 0
+		while cpt < 5:
+			x = random.randint(0,10)
+			y = random.randint(0,10)
+						
+			if self.__grid[y][x].isObs == False :
+				self.__grid[y][x].isRess = True
+				cpt += 1
+			
+	
+	def __init__(self):
+		"""
+		_ _ _ _ _ _ _ _ _ _ _
+		_ X X X _ _ _ _ _ _ _
+		_ _ _ _ _ _ _ X X X X
+		_ _ _ _ _ _ _ _ _ X _
+		_ _ _ _ _ _ _ _ _ X _
+		_ _ _ _ X X _ _ _ _ _
+		_ _ _ _ X _ _ _ _ _ _
+		_ X _ _ _ _ _ X _ X _
+		_ X X _ _ _ _ X X X _
+		_ _ X _ _ _ X X _ _ _
+		_ _ _ _ _ _ _ _ _ _ _
+		"""
+		self.__grid = [
+			[Tiles(0,0,False), Tiles(0,1,False), Tiles(0,2,False), Tiles(0,3,False), Tiles(0,4,False), Tiles(0,5,False), Tiles(0,6,False), Tiles(0,7,False), Tiles(0,8,False), Tiles(0,9,False), Tiles(0,10,False)],
+			[Tiles(1,0,False), Tiles(1,1,True),  Tiles(1,2,True),  Tiles(1,3,True),  Tiles(1,4,False), Tiles(1,5,False), Tiles(1,6,False), Tiles(1,7,False), Tiles(1,8,False), Tiles(1,9,False), Tiles(1,10,False)],
+			[Tiles(2,0,False), Tiles(2,1,False), Tiles(2,2,False), Tiles(2,3,False), Tiles(2,4,False), Tiles(2,5,False), Tiles(2,6,False), Tiles(2,7,True),  Tiles(2,8,True),  Tiles(2,9,True),  Tiles(2,10,True)],
+			[Tiles(3,0,False), Tiles(3,1,False), Tiles(3,2,False), Tiles(3,3,False), Tiles(3,4,False), Tiles(3,5,False), Tiles(3,6,False), Tiles(3,7,False), Tiles(3,8,False), Tiles(3,9,True),  Tiles(3,10,False)],
+			[Tiles(4,0,False), Tiles(4,1,False), Tiles(4,2,False), Tiles(4,3,False), Tiles(4,4,False), Tiles(4,5,False), Tiles(4,6,False), Tiles(4,7,False), Tiles(4,8,False), Tiles(4,9,True),  Tiles(4,10,False)],
+			[Tiles(5,0,False), Tiles(5,1,False), Tiles(5,2,False), Tiles(5,3,False), Tiles(5,4,True),  Tiles(5,5,True),  Tiles(5,6,False), Tiles(5,7,False), Tiles(5,8,False), Tiles(5,9,False), Tiles(5,10,False)],
+			[Tiles(6,0,False), Tiles(6,1,False), Tiles(6,2,False), Tiles(6,3,False), Tiles(6,4,True),  Tiles(6,5,False), Tiles(6,6,False), Tiles(6,7,False), Tiles(6,8,False), Tiles(6,9,False), Tiles(6,10,False)],
+			[Tiles(7,0,False), Tiles(7,1,True),  Tiles(7,2,False), Tiles(7,3,False), Tiles(7,4,False), Tiles(7,5,False), Tiles(7,6,False), Tiles(7,7,True),  Tiles(7,8,False), Tiles(7,9,True),  Tiles(7,10,False)],
+			[Tiles(8,0,False), Tiles(8,1,True),  Tiles(8,2,True),  Tiles(8,3,False), Tiles(8,4,False), Tiles(8,5,False), Tiles(8,6,False), Tiles(8,7,True),  Tiles(8,8,True),  Tiles(8,9,True),  Tiles(8,10,False)],
+			[Tiles(9,0,False), Tiles(9,1,False), Tiles(9,2,True),  Tiles(9,3,False), Tiles(9,4,False), Tiles(9,5,False), Tiles(9,6,True),  Tiles(9,7,True),  Tiles(9,8,False), Tiles(9,9,False), Tiles(9,10,False)],
+			[Tiles(10,0,False), Tiles(10,1,False), Tiles(10,2,False), Tiles(10,3,False), Tiles(10,4,False),  Tiles(10,5,False), Tiles(10,6,False), Tiles(10,7,False), Tiles(10,8,False), Tiles(10,9,False), Tiles(10,10,False)]
+			]
+		self.__listRobot = {}
+		self.refreshRess()
+		
+	@property
+	def listRobot(self) : return self.__listRobot
+	
+	def __str__(self):
+		ret = ""
+		for lig in self.__grid:
+			ret += "|"
+			for col in lig:
+				isRob = False
+				for robot in self.__listRobot:
+   					if self.__listRobot[robot].posX == col and self.__listRobot[robot].posY == lig and self.__listRobot[robot].actif:
+					    isRob = True
+				if isRob :
+					ret+= "-|"
+				else:
+					ret+= str(col) +"|"
+			ret += "\n"
+		return ret
+                    
+	def getPos(self,x,y):
+		return self.__grid[x][y]
+        
--- SIBADE-SUSA/server.py
+++ SIBADE-SUSA/server.py
@@ -1,36 +1,27 @@
 from socket import *
 import sys, threading, os, os.path, re, datetime, configparser
-from spaceX import Map,Robot
+from lib import Map,Robot
 
 if len(sys.argv) != 1:
     print(f"Usage: {sys.argv[0]}")
     sys.exit(1)
 
+def updateLog(code):
+	with open(config['DEFAULT']['log'], "a") as logFic:
+		logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse " + code)
+
 def initRobot(client):
     mapServ.listRobot[client] = Robot(0,0,"newRobot")
-
-def quiter(client):
-	mapServ.listRobot[client].actif = False
-	with open(config['DEFAULT']['log'], "a") as logFic:
-		logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 1011")
-	return "1011"
 
 def rename(client,cmd):
     newName = cmd.decode("utf-8").split()
     if len(newName) != 2 :
-        with open(config['DEFAULT']['log'], "a") as logFic:
-            logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 3021")
         return "3021"
     for rob in mapServ.listRobot:
         if mapServ.listRobot[rob].name == newName[1]:
-            with open(config['DEFAULT']['log'], "a") as logFic:
-                logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 2021")
             return "2021"
 
     mapServ.listRobot[client].name = newName[1]
-    print(mapServ.listRobot[client])
-    with open(config['DEFAULT']['log'], "a") as logFic:
-        logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 1021")
     return "1021"
 
 def send(client,cmd):
@@ -39,27 +30,19 @@ def send(client,cmd):
 def pause(client):
     if mapServ.listRobot[client].actif:
         mapServ.listRobot[client].actif = False
-        with open(config['DEFAULT']['log'], "a") as logFic:
-            logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 1041")
         return "1041"
     else:
-        with open(config['DEFAULT']['log'], "a") as logFic:
-            logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 2041")
         return "2041"
 
 def unpause(client):
     if mapServ.listRobot[client].actif:
-        with open(config['DEFAULT']['log'], "a") as logFic:
-            logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 2051")
-        return "1041"
+        return "2051"
     else:
         mapServ.listRobot[client].actif = True
-        with open(config['DEFAULT']['log'], "a") as logFic:
-            logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 1051")
-        return "2041"
+        return "1051"
 
 def status(client):
-    return "6"
+    return "1061_\n"+str(mapServ.listRobot[client])
 
 def info(client):
     info = "\n"
@@ -67,15 +50,45 @@ def info(client):
         info = "no robots found"
     else:
         for rob in mapServ.listRobot:
-            info += str(mapServ.listRobot[rob]) + "\n"
-    with open(config['DEFAULT']['log'], "a") as logFic:
-	    logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Reponse 1071")
+            if mapServ.listRobot[rob].actif:
+                info += str(mapServ.listRobot[rob]) + "\n"
     return "1071_"+info
 
-def move(client,cmd):
-    return "8"
+def move(client,cmd,mapServ):
+	direc = cmd.decode("utf-8").split()
+	
+	if len(direc) != 2 :
+		return "3081"
+	if direc[1].upper() not in {'U','D','L','R'}:
+		return "3082"
+    
+	newCoord = (0,0)
+    
+	if direc[1].upper() == 'U':
+		newCoord = (-1,0)
+	if direc[1].upper() == 'D':
+		newCoord = (1,0)
+	if direc[1].upper() == 'L':
+		newCoord = (0,-1)
+	if direc[1].upper() == 'D':
+		newCoord = (0,1)
+	
+	oldCoord = (mapServ.listRobot[client].posX,mapServ.listRobot[client].posY)
 
-def switch(client,cmd):
+	if mapServ.getPos((newCoord[0]+oldCoord[0]),(newCoord[1]+oldCoord[1])).isObs or newCoord[0]+oldCoord[0] < 0 or newCoord[0]+oldCoord[0] > 11 or newCoord[1]+oldCoord[1] < 0 or newCoord[1]+oldCoord[1] > 10 :
+		return "2081"
+	else:
+		#voir si case est ress
+		#if mapServ.getPos((newCoord[0]+oldCoord[0]),(newCoord[1]+oldCoord[1])).isRess:
+		mapServ.listRobot[client].posX = (newCoord[0]+oldCoord[0])
+		mapServ.listRobot[client].posY = (newCoord[1]+oldCoord[1])
+		return "1081"
+
+def quiter(client):
+	mapServ.listRobot[client].actif = False
+	return "1011"
+
+def switch(client,cmd,mapServ):
     x = cmd.decode("utf-8").split()[0].upper()
     with open(config['DEFAULT']['log'], "a") as logFic:
             logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Commande " + x + " par " + client )
@@ -87,7 +100,7 @@ def switch(client,cmd):
         'UNPAUSE': unpause(client),
         'STATUS': status(client),
         'INFO': info(client),
-        'MOVE': move(client,cmd)
+        'MOVE': move(client,cmd,mapServ)
     }.get(x, "4000")
 
 # Prog principal
@@ -118,11 +131,14 @@ while True:
             logFic.write("\n" + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S") + " Connexion de " + adr_client[0])
 
         cmd = sock_client.recv(255)
-        rep = switch(adr_client[0],cmd)
+        rep = switch(adr_client[0],cmd,mapServ)
         print(rep)
         print(mapServ)
+        print(mapServ.listRobot[adr_client[0]])
+        print("_____")
+        updateLog(rep.split("_")[0])
         #return nouvelle Map
-        sock_server.sendto(rep.encode(), adr_client)
+        #sock_server.sendto(rep.encode(), adr_client)
 
     except KeyboardInterrupt:
         break
